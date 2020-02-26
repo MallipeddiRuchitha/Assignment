aedd7c7798ec363e06cef994865e82c1cfef3723
MallipeddiRuchitha <59605431+MallipeddiRuchitha@users.noreply.github.com>
2020-02-26 15:30:33 +0530

    modifiedTestCycleClass

--- java/A#7/2/cycle/TestCycle.java
+++ java/A#7/2/cycle/TestCycle.java
@@ -6,23 +6,24 @@
 public class TestCycle {
     public static void main(String[] args){
         Cycle [] cycleArray=new Cycle[3];
-        cycleArray[0]=(Cycle)new Unicycle();
-        cycleArray[1]=(Cycle)new Bicycle();
-        cycleArray[2]=(Cycle)new Tricycle();
+        cycleArray[0]=new Unicycle();
+        cycleArray[1]=new Bicycle();
+        cycleArray[2]=new Tricycle();
         System.out.println("After Upcasting");
-        //the following 3 statements  will give  "cannot resolve balance in cycle"  error
+        //the following 3 statements  will give  "cannot resolve balance in cycle"  error because balance method is not there in Cycle class.
         cycleArray[0].balance();
         cycleArray[1].balance();
         cycleArray[2].balance();
 
 
 
-        cycleArray[0]=new Unicycle();
-        cycleArray[1]=new Bicycle();
-        cycleArray[2]=new Tricycle();
-        //the following 3 statements  will give  "cannot resolve balance in cycle"  error
+        cycleArray[0]=(Unicycle)cycleArray[0];
+        cycleArray[1]=(Bicycle)cycleArray[1];
+        cycleArray[2]=(Tricycle)cycleArray[2];
+      
         cycleArray[0].balance();
         cycleArray[1].balance();
+            //the following statement will give an error because there is no balance method in TriCycle class.
         cycleArray[2].balance();
 
 
