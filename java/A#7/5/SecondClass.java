48edd1770f2cc4668d737ed9283fc8dbc176c68c
MallipeddiRuchitha <59605431+MallipeddiRuchitha@users.noreply.github.com>
2020-02-26 16:21:04 +0530

    modifiedSecondClass

--- java/A#7/5/SecondClass.java
+++ java/A#7/5/SecondClass.java
@@ -1,8 +1,23 @@
 package assignment;
 
 public class SecondClass {
-    //it gives errors such as no default constructur in FirstClass.InnerFirstClass,no enclosing instance of type FirstClass.InnerFirstClass is in scope
+
     public class InnerSecondClass extends FirstClass.InnerFirstClass {
+        public InnerSecondClass(FirstClass firstClassObj ){
+            //when u call super, constructor of FirstInnerClass which is a inner class of FirstClass is called;
+            firstClassObj.super("inner second class");
+
+        }
+
+    }
+    public static void main(String[] args){
+
+        FirstClass firstClassObj=new FirstClass();
+
+        SecondClass secondClassObj=new SecondClass();
+
+
+        secondClassObj.new InnerSecondClass(firstClassObj);
 
     }
 }
